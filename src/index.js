e9891fe62433b60ccdb45579b32cbf55edfd4f7f
BrianGustafBirahy <s22110506@student.unklab.ac.id>
2024-04-22 20:49:16 +0800

    mengurangi yang tidak perlu

--- src/index.js
+++ src/index.js
@@ -9,19 +9,9 @@ dotenv.config();
 
 const port = process.env.PORT;
 const hostname = "127.0.0.1";
-
-// app.get("/api",(req, res)=> {
-//   res.send("Hello World!")
-// })
-
-// app.get("/mahasiswa",async (req, res)=> {
-//   const mahasiswaAll = await prisma.mahasiswa.findMany;
-//   res.send(mahasiswaAll);
-// })
-
+//  Get all mahasiswa
 app.get("/mahasiswa", async (req, res) => {
   try {
-    // const result = await db.query("SELECT * FROM students");
     const allMahasiswa = await prisma.mahasiswa.findMany();
     console.log(allMahasiswa);
     res.status(200).json({
@@ -60,7 +50,6 @@ app.get("/mahasiswa", async (req, res) => {
 ////////////////////////// Masih error
 
 // Cari mahasiswa by id
-
 app.get("/mahasiswa/:id_mhs", async (req, res) => {
   try {
     const { id_mhs } = req.params;
@@ -74,48 +63,6 @@ app.get("/mahasiswa/:id_mhs", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ====================================================================================================================
-// app.get("/", (req, res) => res.send("Hello World"));
-// app.get("/about", (req, res) =>
-//   res.status(200).json({
-//     status: "success",
-//     message: "About Page",
-//     data: [],
-//   })
-// );
-// app.post("/contoh", (req, res) => res.send("Request dengan method POST"));
-
-// app.put("/contoh", (req, res) => res.send("Request dengan method PUT"));
-
-// app.patch("/contoh", (req, res) => res.send("Request dengan method PATCH"));
-
-// app.delete("/contoh", (req, res) => res.send("Request dengan method DELETE"));
-
-// //Params
-// app.get("/post/:id", (req, res) => {
-//   const id = req.params.id;
-//   res.send(`Post dengan ID ${id}`);
-// });
-// //Query string
-// app.get("/post", (req, res) => {
-//   const { page } = req.query;
-//   res.send(`Data yang didapatkan adalah ${page}`);
-// });
-
-// ====================================================================================================================
 
 app.listen(port, () =>
   console.log(`Server running at http://${hostname}:${port}`)
